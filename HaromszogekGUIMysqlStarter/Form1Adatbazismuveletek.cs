0ce6bb5e1d1df79ed147173b7e6fad4caa62c297
Csaba Gyuris <gycsaba@vasvari.hu>
2019-10-08 15:16:17 +0200

    Projekt GUI adatbázis fejlesztéshez

---
+++ HaromszogekGUIMysqlStarter/Form1Adatbazismuveletek.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HaromszogekSzoftverfejleszto.Modell;
+using HaromszogekSzoftverfejleszto.Tarolo;
+
+using HaromszogekGUI.Tarolo;
+
+namespace HaromszogekGUI
+{
+    public partial class Form1 : Form
+    {
+        HaromszogekDatabase ham = new HaromszogekDatabase();
+
+        private void buttonUresAdatbazisLetrehozasa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonAdatbazisbaUresTablaLetrehozasa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonMentAdatokatAdatbazisba_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonOsszesAdatTorlese_Click(object sender, EventArgs e)
+        {
+            haromszogek.torolOsszesAdat();
+            listBoxHaromszogek.DataSource = null;
+        }
+
+        private void buttonTablaTorlese_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonAdatbazisTorlese_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ HaromszogekGUIMysqlStarter/Tarolo/HaromszogekDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using HaromszogekSzoftverfejleszto.Modell;
+
+namespace HaromszogekGUI.Tarolo
+{
+    class HaromszogekDatabase
+    {
+        private readonly string connectionStringCreate;
+        private readonly string connectionString;
+
+        public HaromszogekDatabase()
+        {
+            connectionStringCreate =
+               "SERVER=\"localhost\";"
+               + "DATABASE=\"test\";"
+               + "UID=\"root\";"
+               + "PASSWORD=\"\";"
+               + "PORT=\"3307\";";
+
+            connectionString =
+                "SERVER=\"localhost\";"
+                + "DATABASE=\"haromszogek\";"
+                + "UID=\"root\";"
+                + "PASSWORD=\"\";"
+                + "PORT=\"3307\";";
+        }     
+    }
+}
